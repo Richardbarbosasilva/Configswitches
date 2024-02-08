af7b1ede5cd2c8fe7023ebfb1bc6717514f3c463
Richard Barbosa Silva <112967185+Richardbarbosasilva@users.noreply.github.com>
2024-02-08 15:29:46 -0400

    Create default.js

---
+++ default.js
@@ -0,0 +1,7 @@
+// função para chamar index2.html a partir do evento click no botão
+
+function redirecionarParaOutraPagina() {
+    // Substitua "outra_pagina.html" pelo nome da sua outra página
+    window.location.href = "index2.html";
+
+}
